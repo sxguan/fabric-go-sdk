75f7f7a9ff31b4034b7fa1ba083afa0a7d54a062
sxguan <54309997+sxguan@users.noreply.github.com>
2022-03-26 16:18:55 +0800

    Update integration.go

--- sdkInit/integration.go
+++ sdkInit/integration.go
@@ -1,18 +1,17 @@
 package sdkInit
 
-
-
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/event"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/status"
 	contextAPI "github.com/hyperledger/fabric-sdk-go/pkg/common/providers/context"
 	fabAPI "github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
 	contextImpl "github.com/hyperledger/fabric-sdk-go/pkg/context"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
-
-	"time"
+	"log"
 )
 
 func DiscoverLocalPeers(ctxProvider contextAPI.ClientProvider, expectedPeers int) ([]fabAPI.Peer, error) {
@@ -39,37 +38,99 @@ func DiscoverLocalPeers(ctxProvider contextAPI.ClientProvider, expectedPeers int
 
 	return discoveredPeers.([]fabAPI.Peer), nil
 }
-func (t *SdkEnvInfo)InitService(chaincodeID, channelID string, org *OrgInfo, sdk *fabsdk.FabricSDK) error{
+func (t *SdkEnvInfo) InitService(chaincodeID, channelID string, org *OrgInfo, sdk *fabsdk.FabricSDK) error {
 	handler := &SdkEnvInfo{
-		ChaincodeID:chaincodeID,
+		ChaincodeID: chaincodeID,
 	}
 	//prepare channel client context using client context
 	clientChannelContext := sdk.ChannelContext(channelID, fabsdk.WithUser(org.OrgUser), fabsdk.WithOrg(org.OrgName))
 	// Channel client is used to query and execute transactions (Org1 is default org)
 	var err error
-	t.Client,err= channel.New(clientChannelContext)
+	t.ChClient, err = channel.New(clientChannelContext)
 	if err != nil {
-		return nil
+		return err
 	}
-	handler.Client = t.Client
+	t.EvClient, err = event.New(clientChannelContext, event.WithBlockEvents())
+	if err != nil {
+		return err
+	}
+	handler.ChClient = t.ChClient
+	handler.EvClient = t.EvClient
 	return nil
 }
 
-func regitserEvent(client *channel.Client, chaincodeID, eventID string) (fabAPI.Registration, <-chan *fabAPI.CCEvent) {
+func regitserEvent(client *event.Client, chaincodeID string) (fabAPI.Registration, <-chan *fabAPI.CCEvent) {
+	eventName := "chaincode-event"
 
-	reg, notifier, err := client.RegisterChaincodeEvent(chaincodeID, eventID)
+	reg, notifier, err := client.RegisterChaincodeEvent(chaincodeID, eventName)
 	if err != nil {
 		fmt.Println("注册链码事件失败: %s", err)
 	}
+
 	return reg, notifier
 }
+func ChainCodeEventListener(c *event.Client, ccID string) fabAPI.Registration {
 
-func eventResult(notifier <-chan *fabAPI.CCEvent, eventID string) error {
-	select {
-	case ccEvent := <-notifier:
-		fmt.Printf("接收到链码事件: %v\n", ccEvent)
-	case <-time.After(time.Second * 20):
-		return fmt.Errorf("不能根据指定的事件ID接收到相应的链码事件(%s)", eventID)
+	reg, notifier := regitserEvent(c, ccID)
+
+	// consume event
+	go func() {
+		for e := range notifier {
+			log.Printf("Receive cc event, ccid: %v \neventName: %v\n"+
+				"payload: %v \ntxid: %v \nblock: %v \nsourceURL: %v\n",
+				e.ChaincodeID, e.EventName, string(e.Payload), e.TxID, e.BlockNumber, e.SourceURL)
+		}
+	}()
+
+	return reg
+}
+
+func TxListener(c *event.Client, txIDCh chan string) {
+	log.Println("Transaction listener start")
+	defer log.Println("Transaction listener exit")
+
+	for id := range txIDCh {
+		// Register monitor transaction event
+		log.Printf("Register transaction event for: %v", id)
+		txReg, txCh, err := c.RegisterTxStatusEvent(id)
+		if err != nil {
+			log.Printf("Register transaction event error: %v", err)
+			continue
+		}
+		defer c.Unregister(txReg)
+
+		// Receive transaction event
+		go func() {
+			for e := range txCh {
+				log.Printf("Receive transaction event: txid: %v, "+
+					"validation code: %v, block number: %v",
+					e.TxID,
+					e.TxValidationCode,
+					e.BlockNumber)
+			}
+		}()
 	}
-	return nil
+}
+
+func BlockListener(ec *event.Client) fabAPI.Registration {
+	// Register monitor block event
+	beReg, beCh, err := ec.RegisterBlockEvent()
+	if err != nil {
+		log.Printf("Register block event error: %v", err)
+	}
+	log.Println("Registered block event")
+
+	// Receive block event
+	go func() {
+		for e := range beCh {
+			log.Printf("Receive block event:\nSourceURL: %v\nNumber: %v\nHash"+
+				": %v\nPreviousHash: %v\n\n",
+				e.SourceURL,
+				e.Block.Header.Number,
+				hex.EncodeToString(e.Block.Header.DataHash),
+				hex.EncodeToString(e.Block.Header.PreviousHash))
+		}
+	}()
+
+	return beReg
 }
